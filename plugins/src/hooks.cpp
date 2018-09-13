07238a21922f73ea707bf923ec214ee62dcd49f5
IllidanS4 <illidans4@gmail.com>
2018-09-13 13:35:43 +0200

    simplified hooks

--- plugins/src/hooks.cpp
+++ plugins/src/hooks.cpp
@@ -14,155 +14,60 @@
 
 extern void *pAMXFunctions;
 
-subhook_t amx_Init_h;
-subhook_t amx_Register_h;
-subhook_t amx_Exec_h;
-subhook_t amx_FindPublic_h;
-subhook_t amx_GetPublic_h;
-subhook_t amx_NumPublics_h;
-subhook_t amx_FindPubVar_h;
-subhook_t amx_GetPubVar_h;
-subhook_t amx_NumPubVars_h;
-subhook_t amx_FindTagId_h;
-subhook_t amx_GetTag_h;
-subhook_t amx_NumTags_h;
-subhook_t amx_GetAddr_h;
+template <class FType>
+class amx_hook_func;
 
-int AMXAPI amx_InitOrig(AMX *amx, void *program)
+template <class Ret, class... Args>
+class amx_hook_func<Ret(*)(Args...)>
 {
-	if(subhook_is_installed(amx_Init_h))
-	{
-		return reinterpret_cast<decltype(&amx_Init)>(subhook_get_trampoline(amx_Init_h))(amx, program);
-	}else{
-		return amx_Init(amx, program);
-	}
-}
+public:
+	typedef Ret hook_ftype(Ret(*)(Args...), Args...);
 
-int AMXAPI amx_ExecOrig(AMX *amx, cell *retval, int index)
+	template <subhook_t &Hook, hook_ftype *Handler>
+	static Ret AMXAPI handler(Args... args)
+	{
+		return Handler(reinterpret_cast<Ret(*)(Args...)>(subhook_get_trampoline(Hook)), args...);
+	}
+};
+
+template <int Index, class FType, typename amx_hook_func<FType>::hook_ftype Func>
+class amx_hook
 {
-	if(subhook_is_installed(amx_Exec_h))
-	{
-		return reinterpret_cast<decltype(&amx_Exec)>(subhook_get_trampoline(amx_Exec_h))(amx, retval, index);
-	}else{
-		return amx_Exec(amx, retval, index);
-	}
-}
+	static subhook_t hook;
 
-int AMXAPI amx_RegisterOrig(AMX *amx, const AMX_NATIVE_INFO *nativelist, int number)
-{
-	if(subhook_is_installed(amx_Register_h))
+public:
+	static void load()
 	{
-		return reinterpret_cast<decltype(&amx_Register)>(subhook_get_trampoline(amx_Register_h))(amx, nativelist, number);
-	} else {
-		return amx_Register(amx, nativelist, number);
+		hook = subhook_new(reinterpret_cast<void*>(((FType*)pAMXFunctions)[Index]), reinterpret_cast<void*>(&amx_hook_func<FType>::handler<hook, Func>), {});
+		subhook_install(hook);
 	}
-}
 
-int AMXAPI amx_FindPublicOrig(AMX *amx, const char *funcname, int *index)
-{
-	if(subhook_is_installed(amx_FindPublic_h))
+	static void unload()
 	{
-		return reinterpret_cast<decltype(&amx_FindPublicOrig)>(subhook_get_trampoline(amx_FindPublic_h))(amx, funcname, index);
-	}else{
-		return amx_FindPublic(amx, funcname, index);
+		subhook_remove(hook);
+		subhook_free(hook);
 	}
-}
 
-int AMXAPI amx_GetPublicOrig(AMX *amx, int index, char *funcname)
-{
-	if(subhook_is_installed(amx_GetPublic_h))
+	static FType orig()
 	{
-		return reinterpret_cast<decltype(&amx_GetPublicOrig)>(subhook_get_trampoline(amx_GetPublic_h))(amx, index, funcname);
-	}else{
-		return amx_GetPublic(amx, index, funcname);
+		if(subhook_is_installed(hook))
+		{
+			return reinterpret_cast<FType>(subhook_get_trampoline(hook));
+		}else{
+			return ((FType*)pAMXFunctions)[Index];
+		}
 	}
-}
+};
 
-int AMXAPI amx_NumPublicsOrig(AMX *amx, int *number)
-{
-	if(subhook_is_installed(amx_NumPublics_h))
-	{
-		return reinterpret_cast<decltype(&amx_NumPublicsOrig)>(subhook_get_trampoline(amx_NumPublics_h))(amx, number);
-	}else{
-		return amx_NumPublics(amx, number);
-	}
-}
+template <int index, class FType, typename amx_hook_func<FType>::hook_ftype Func>
+subhook_t amx_hook<index, FType, Func>::hook;
 
-int AMXAPI amx_FindPubVarOrig(AMX *amx, const char *varname, cell *amx_addr)
-{
-	if(subhook_is_installed(amx_FindPubVar_h))
-	{
-		return reinterpret_cast<decltype(&amx_FindPubVarOrig)>(subhook_get_trampoline(amx_FindPubVar_h))(amx, varname, amx_addr);
-	} else{
-		return amx_FindPubVar(amx, varname, amx_addr);
-	}
-}
-
-int AMXAPI amx_GetPubVarOrig(AMX *amx, int index, char *varname, cell *amx_addr)
-{
-	if(subhook_is_installed(amx_GetPubVar_h))
-	{
-		return reinterpret_cast<decltype(&amx_GetPubVarOrig)>(subhook_get_trampoline(amx_GetPubVar_h))(amx, index, varname, amx_addr);
-	} else{
-		return amx_GetPubVar(amx, index, varname, amx_addr);
-	}
-}
-
-int AMXAPI amx_NumPubVarsOrig(AMX *amx, int *number)
-{
-	if(subhook_is_installed(amx_NumPubVars_h))
-	{
-		return reinterpret_cast<decltype(&amx_NumPubVarsOrig)>(subhook_get_trampoline(amx_NumPubVars_h))(amx, number);
-	} else{
-		return amx_NumPubVars(amx, number);
-	}
-}
-
-int AMXAPI amx_FindTagIdOrig(AMX *amx, cell tag_id, char *tagname)
-{
-	if(subhook_is_installed(amx_FindTagId_h))
-	{
-		return reinterpret_cast<decltype(&amx_FindTagIdOrig)>(subhook_get_trampoline(amx_FindTagId_h))(amx, tag_id, tagname);
-	}else{
-		return amx_FindTagId(amx, tag_id, tagname);
-	}
-}
-
-int AMXAPI amx_GetTagOrig(AMX *amx, int index, char *tagname, cell *tag_id)
-{
-	if(subhook_is_installed(amx_GetTag_h))
-	{
-		return reinterpret_cast<decltype(&amx_GetTagOrig)>(subhook_get_trampoline(amx_GetTag_h))(amx, index, tagname, tag_id);
-	}else{
-		return amx_GetTag(amx, index, tagname, tag_id);
-	}
-}
-
-int AMXAPI amx_NumTagsOrig(AMX *amx, int *number)
-{
-	if(subhook_is_installed(amx_NumTags_h))
-	{
-		return reinterpret_cast<decltype(&amx_NumTagsOrig)>(subhook_get_trampoline(amx_NumTags_h))(amx, number);
-	}else{
-		return amx_NumTags(amx, number);
-	}
-}
-
-int AMXAPI amx_GetAddrOrig(AMX *amx, cell amx_addr, cell **phys_addr)
-{
-	if(subhook_is_installed(amx_GetAddr_h))
-	{
-		return reinterpret_cast<decltype(&amx_GetAddrOrig)>(subhook_get_trampoline(amx_GetAddr_h))(amx, amx_addr, phys_addr);
-	}else{
-		return amx_GetAddr(amx, amx_addr, phys_addr);
-	}
-}
-
+#define AMX_HOOK_FUNC(Func, ...) Func(decltype(&::Func) orig, __VA_ARGS__)
 namespace hooks
 {
-	int AMXAPI amx_Init(AMX *amx, void *program)
+	int AMX_HOOK_FUNC(amx_Init, AMX *amx, void *program)
 	{
-		int ret = amx_InitOrig(amx, program);
+		int ret = orig(amx, program);
 		if(ret == AMX_ERR_NONE)
 		{
 			amx::loader::Init(amx, program);
@@ -170,14 +75,14 @@ namespace hooks
 		return ret;
 	}
 
-	int AMXAPI amx_Exec(AMX *amx, cell *retval, int index)
+	int AMX_HOOK_FUNC(amx_Exec, AMX *amx, cell *retval, int index)
 	{
 		int result;
 		if(lua::interop::amx_exec(amx, retval, index, result))
 		{
 			return result;
 		}
-		int error = amx_ExecOrig(amx, retval, index);
+		int error = orig(amx, retval, index);
 		if(error == AMX_ERR_SLEEP)
 		{
 			return lua::bind(amx, retval, index);
@@ -185,14 +90,14 @@ namespace hooks
 		return error;
 	}
 
-	int AMXAPI amx_Register(AMX *amx, const AMX_NATIVE_INFO *nativelist, int number)
+	int AMX_HOOK_FUNC(amx_Register, AMX *amx, const AMX_NATIVE_INFO *nativelist, int number)
 	{
-		int ret = amx_RegisterOrig(amx, nativelist, number);
+		int ret = orig(amx, nativelist, number);
 		lua::interop::amx_register_natives(amx, nativelist, number);
 		return ret;
 	}
 
-	int AMXAPI amx_FindPublic(AMX *amx, const char *funcname, int *index)
+	int AMX_HOOK_FUNC(amx_FindPublic, AMX *amx, const char *funcname, int *index)
 	{
 		int error;
 		if(lua::interop::amx_find_public(amx, funcname, index, error))
@@ -200,30 +105,30 @@ namespace hooks
 			return error;
 		}
 
-		return amx_FindPublicOrig(amx, funcname, index);
+		return orig(amx, funcname, index);
 	}
 
-	int AMXAPI amx_GetPublic(AMX *amx, int index, char *funcname)
+	int AMX_HOOK_FUNC(amx_GetPublic, AMX *amx, int index, char *funcname)
 	{
 		if(lua::interop::amx_get_public(amx, index, funcname))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_GetPublicOrig(amx, index, funcname);
+		return orig(amx, index, funcname);
 	}
 
-	int AMXAPI amx_NumPublics(AMX *amx, int *number)
+	int AMX_HOOK_FUNC(amx_NumPublics, AMX *amx, int *number)
 	{
 		if(lua::interop::amx_num_publics(amx, number))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_NumPublicsOrig(amx, number);
+		return orig(amx, number);
 	}
 
-	int AMXAPI amx_FindPubVar(AMX *amx, const char *varname, cell *amx_addr)
+	int AMX_HOOK_FUNC(amx_FindPubVar, AMX *amx, const char *varname, cell *amx_addr)
 	{
 		int error;
 		if(lua::interop::amx_find_pubvar(amx, varname, amx_addr, error))
@@ -231,113 +136,102 @@ namespace hooks
 			return error;
 		}
 
-		return amx_FindPubVarOrig(amx, varname, amx_addr);
+		return orig(amx, varname, amx_addr);
 	}
 
-	int AMXAPI amx_GetPubVar(AMX *amx, int index, char *varname, cell *amx_addr)
+	int AMX_HOOK_FUNC(amx_GetPubVar, AMX *amx, int index, char *varname, cell *amx_addr)
 	{
 		if(lua::interop::amx_get_pubvar(amx, index, varname, amx_addr))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_GetPubVarOrig(amx, index, varname, amx_addr);
+		return orig(amx, index, varname, amx_addr);
 	}
 
-	int AMXAPI amx_NumPubVars(AMX *amx, int *number)
+	int AMX_HOOK_FUNC(amx_NumPubVars, AMX *amx, int *number)
 	{
 		if(lua::interop::amx_num_pubvars(amx, number))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_NumPubVarsOrig(amx, number);
+		return orig(amx, number);
 	}
 
-	int AMXAPI amx_FindTagId(AMX *amx, cell tag_id, char *tagname)
+	int AMX_HOOK_FUNC(amx_FindTagId, AMX *amx, cell tag_id, char *tagname)
 	{
 		if(lua::interop::amx_find_tag_id(amx, tag_id, tagname))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_FindTagIdOrig(amx, tag_id, tagname);
+		return orig(amx, tag_id, tagname);
 	}
 
-	int AMXAPI amx_GetTag(AMX *amx, int index, char *tagname, cell *tag_id)
+	int AMX_HOOK_FUNC(amx_GetTag, AMX *amx, int index, char *tagname, cell *tag_id)
 	{
 		if(lua::interop::amx_get_tag(amx, index, tagname, tag_id))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_GetTagOrig(amx, index, tagname, tag_id);
+		return orig(amx, index, tagname, tag_id);
 	}
 
-	int AMXAPI amx_NumTags(AMX *amx, int *number)
+	int AMX_HOOK_FUNC(amx_NumTags, AMX *amx, int *number)
 	{
 		if(lua::interop::amx_num_tags(amx, number))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_NumTagsOrig(amx, number);
+		return orig(amx, number);
 	}
 
-	int AMXAPI amx_GetAddr(AMX *amx, cell amx_addr, cell **phys_addr)
+	int AMX_HOOK_FUNC(amx_GetAddr, AMX *amx, cell amx_addr, cell **phys_addr)
 	{
 		if(lua::interop::amx_get_addr(amx, amx_addr, phys_addr))
 		{
 			return AMX_ERR_NONE;
 		}
 
-		return amx_GetAddrOrig(amx, amx_addr, phys_addr);
+		return orig(amx, amx_addr, phys_addr);
 	}
 }
 
-template <class Func>
-void register_amx_hook(subhook_t &hook, int index, Func *fnptr)
-{
-	hook = subhook_new(reinterpret_cast<void*>(((Func**)pAMXFunctions)[index]), reinterpret_cast<void*>(fnptr), {});
-	subhook_install(hook);
-}
+#define amx_Hook(Func) amx_hook<PLUGIN_AMX_EXPORT_##Func, decltype(&::amx_##Func), &hooks::amx_##Func>
 
 void hooks::load()
 {
-	register_amx_hook(amx_Init_h, PLUGIN_AMX_EXPORT_Init, &hooks::amx_Init);
-	register_amx_hook(amx_Register_h, PLUGIN_AMX_EXPORT_Register, &hooks::amx_Register);
-	register_amx_hook(amx_Exec_h, PLUGIN_AMX_EXPORT_Exec, &hooks::amx_Exec);
-	register_amx_hook(amx_FindPublic_h, PLUGIN_AMX_EXPORT_FindPublic, &hooks::amx_FindPublic);
-	register_amx_hook(amx_GetPublic_h, PLUGIN_AMX_EXPORT_GetPublic, &hooks::amx_GetPublic);
-	register_amx_hook(amx_NumPublics_h, PLUGIN_AMX_EXPORT_NumPublics, &hooks::amx_NumPublics);
-	register_amx_hook(amx_FindPubVar_h, PLUGIN_AMX_EXPORT_FindPubVar, &hooks::amx_FindPubVar);
-	register_amx_hook(amx_GetPubVar_h, PLUGIN_AMX_EXPORT_GetPubVar, &hooks::amx_GetPubVar);
-	register_amx_hook(amx_NumPubVars_h, PLUGIN_AMX_EXPORT_NumPubVars, &hooks::amx_NumPubVars);
-	register_amx_hook(amx_FindTagId_h, PLUGIN_AMX_EXPORT_FindTagId, &hooks::amx_FindTagId);
-	register_amx_hook(amx_GetTag_h, PLUGIN_AMX_EXPORT_GetTag, &hooks::amx_GetTag);
-	register_amx_hook(amx_NumTags_h, PLUGIN_AMX_EXPORT_NumTags, &hooks::amx_NumTags);
-	register_amx_hook(amx_GetAddr_h, PLUGIN_AMX_EXPORT_GetAddr, &hooks::amx_GetAddr);
-}
-
-void unregister_hook(subhook_t hook)
-{
-	subhook_remove(hook);
-	subhook_free(hook);
+	amx_Hook(Init)::load();
+	amx_Hook(Register)::load();
+	amx_Hook(Exec)::load();
+	amx_Hook(FindPublic)::load();
+	amx_Hook(GetPublic)::load();
+	amx_Hook(NumPublics)::load();
+	amx_Hook(FindPubVar)::load();
+	amx_Hook(GetPubVar)::load();
+	amx_Hook(NumPubVars)::load();
+	amx_Hook(FindTagId)::load();
+	amx_Hook(GetTag)::load();
+	amx_Hook(NumTags)::load();
+	amx_Hook(GetAddr)::load();
 }
 
 void hooks::unload()
 {
-	unregister_hook(amx_Init_h);
-	unregister_hook(amx_Register_h);
-	unregister_hook(amx_Exec_h);
-	unregister_hook(amx_FindPublic_h);
-	unregister_hook(amx_GetPublic_h);
-	unregister_hook(amx_NumPublics_h);
-	unregister_hook(amx_FindPubVar_h);
-	unregister_hook(amx_GetPubVar_h);
-	unregister_hook(amx_NumPubVars_h);
-	unregister_hook(amx_FindTagId_h);
-	unregister_hook(amx_GetTag_h);
-	unregister_hook(amx_NumTags_h);
-	unregister_hook(amx_GetAddr_h);
+	amx_Hook(Init)::unload();
+	amx_Hook(Register)::unload();
+	amx_Hook(Exec)::unload();
+	amx_Hook(FindPublic)::unload();
+	amx_Hook(GetPublic)::unload();
+	amx_Hook(NumPublics)::unload();
+	amx_Hook(FindPubVar)::unload();
+	amx_Hook(GetPubVar)::unload();
+	amx_Hook(NumPubVars)::unload();
+	amx_Hook(FindTagId)::unload();
+	amx_Hook(GetTag)::unload();
+	amx_Hook(NumTags)::unload();
+	amx_Hook(GetAddr)::unload();
 }
